6f779718ec811e33986d8861b06c3ca89e752cc6
Andy Nguyen <nguyen3andy@gmail.com>
2023-08-02 19:54:29 -0400

    created nav.link

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -20,7 +20,7 @@ export const NavBar = () => {
     }, [])
 
     return (
-      <Navbar expand="lg">
+      <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
         <Container>
           <Navbar.Brand href="#home">
             <img src={''} alt="Logo"/>
@@ -29,9 +29,9 @@ export const NavBar = () => {
           <span className="navbar-toggler-icon"></span>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#skills">Skills</Nav.Link>
-              <Nav.Link href="#projects">Projects</Nav.Link>
+              <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link'}>Home</Nav.Link>
+              <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link'}>Skills</Nav.Link>
+              <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link'}>Projects</Nav.Link>
             </Nav>
             <span className="navbar-text">
                 <div className="social-icon">
